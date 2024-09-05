d109add488f556aabd018525e45fb3305d6859e4
Yur1V1nn1cyus <158053329+Yur1V1nn1cyus@users.noreply.github.com>
2024-09-05 17:00:23 -0300

    Update databases.py
    Alterações de informações sensiveis

--- CRUD_OPERATION/sql_app/databases.py
+++ CRUD_OPERATION/sql_app/databases.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #Conexão com o banco de dados, verificar a documentação de fastapi
-SQLALCHEMY_DATABASE_URL = "postgresql://yuri:teste123@localhost:8745/db_fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@server:port/dbname"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
